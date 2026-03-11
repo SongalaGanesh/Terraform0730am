a435988136d06da1454d68f30799d1d127f055a2
SongalaGanesh <ganesh.songala@sysintelli.com>
2026-03-11 23:30:29 +0530

    day-4

---
+++ Day-4-statefile/main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "dev" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "dev"
+    }
+}
+
+resource "aws_instance" "test" {
+    ami =  "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.micro"
+    tags = {
+      Name="test-aws_instance"
+    }
+}
