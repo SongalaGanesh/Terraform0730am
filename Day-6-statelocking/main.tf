6738f4e48ab7db4effb202fdbf480ee7adf451c3
SongalaGanesh <ganesh.songala@sysintelli.com>
2026-03-13 16:33:55 +0530

    backend file added

--- Day-6-statelocking/main.tf
+++ Day-6-statelocking/main.tf
@@ -1,6 +1,14 @@
-resource "aws_vpc" "name" {
+resource "aws_vpc" "dev_1" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "tf-State-locking-vpc"
+  }
+}
+
+resource "aws_subnet" "dev_1" {
+  vpc_id     = aws_vpc.dev_1.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "tf-State-locking-subnet"
   }
 }
